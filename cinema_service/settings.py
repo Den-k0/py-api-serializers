159354ef8191d443f0da343a1a08a3ffd73b2d04
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-11 16:40:33 +0200

    Solution fix: remove django-debug-toolbar

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "debug_toolbar",
     "cinema",
     "user",
 ]
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -133,8 +131,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
