093b03fd6ef7ddf3569546a49244a75e1caab14c
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-11 18:23:55 +0200

    add full_name to the read_only_fields tuple

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -20,7 +20,7 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
-        read_only_fields = ("id",)
+        read_only_fields = ("id", "full_name")
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
